fb132d67202842733563ad4751657708a925bee5
Mark Nielsen <mark@moodlerooms.com>
2011-02-24 00:41:05 +0000

    Remove this file
    git-svn-id: http://svn.moodlerooms.com/Moodle/2.0/branches/plugins@13689 f529b7b8-409e-e885-f664-9768b4ed42ab

--- lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function mr_extends_navigation($nav) {
-    // notify('here');
-    // var_dump($nav->get('myprofile'));
-    // print_object($nav);
-    // $usernode = $nav->rootnodes['myprofile'];
-    // $nav->get('myprofile')->add('Testing', new moodle_url('/user/profile.php'), navigation_node::TYPE_CUSTOM, 'short', 0);
-}
