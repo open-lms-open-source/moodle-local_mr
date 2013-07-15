924b5f877f52997f129c8b91cee1020e27268a76
Mark Nielsen <mark@moodlerooms.com>
2013-07-15 13:33:08 -0700

    INT-4892: Prepare plugins for Moodle 2.4 Open Source release
    Updated plugins:
    * blocks/flexpagemod
    * blocks/flexpagenav
    * blocks/intelligent_learning
    * course/format/flexpage
    * course/format/folderview
    * grade/grading/form/checklist
    * local/mr
    * mod/hsuforum
    * theme/flexpage

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  */
 
 $plugin->version   = 2010090201;
-$plugin->requires  = 2012062503;
+$plugin->requires  = 2012120302;
 $plugin->component = 'local_mr';
-$plugin->release   = '2.3.0 (Build: 20121228)';
+$plugin->release   = '2.4.0 (Build: 20130711)';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0;
