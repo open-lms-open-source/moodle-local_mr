bdf3da64614e5b743ab2a0381f8b5d85c63398f9
Mark Nielsen <mark@moodlerooms.com>
2014-01-06 11:02:40 -0800

    INT-5426 & INT-5431: Open Source the MR Framework for Moodle 2.5

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  */
 
 $plugin->version   = 2010090201;
-$plugin->requires  = 2012120302;
+$plugin->requires  = 2013051401;
 $plugin->component = 'local_mr';
-$plugin->release   = '2.4.0 (Build: 20130711)';
+$plugin->release   = '2.5.0 (Build: 20131227)';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0;
