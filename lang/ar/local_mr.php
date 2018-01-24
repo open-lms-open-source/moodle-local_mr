6dd5c484f5c463532c27d04080164176a9b77925
vagrant <vagrant@vagrant.vm>
2018-01-24 16:45:10 +0000

    INT-12838: Importing ar lang

---
+++ lang/ar/local_mr.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'إطار عمل Moodlerooms';
+$string['addedactivity'] = 'تمت إضافة {$a->modname} بواسطة {$a->fullname}';
+$string['basichtml'] = 'HTML الأساسي';
+$string['deletedactivity'] = 'تم حذف {$a->modname} بواسطة {$a->fullname}';
+$string['export'] = 'التصدير: ';
+$string['filter'] = 'التصفية';
+$string['loadingdotdotdot'] = 'يتم الآن التحميل...';
+$string['local_mr_docs'] = 'مستندات';
+$string['moodleroomsframework'] = 'إطار عمل Moodlerooms';
+$string['mrframework'] = 'إطار عمل MR';
+$string['nolimit'] = 'غير محدود';
+$string['oneday'] = 'يوم 1';
+$string['onemonth'] = 'شهر 1';
+$string['oneweek'] = 'أسبوع 1';
+$string['oneyear'] = 'عام 1';
+$string['plugin-spreadsheet/excel'] = 'XLS';
+$string['plugin-spreadsheet/ods'] = 'ODS';
+$string['plugin-text/csv'] = 'CSV';
+$string['plugin-text/tsv'] = 'TSV';
+$string['popupdocs'] = 'فتح مستندات في إطار جديد';
+$string['popuperrors'] = 'فتح أخطاء المستند في إطار جديد';
+$string['reportsql'] = 'تقرير SQL';
+$string['reset'] = 'إعادة تعيين';
+$string['standard'] = 'قياسي';
+$string['updatedactivity'] = 'تم تحديث {$a->modname} بواسطة {$a->fullname}';
+$string['xdays'] = '{$a} أيام';
+$string['xhours'] = '{$a} ساعات';
+$string['xminutes'] = '{$a} دقائق';
+$string['xmonths'] = '{$a} أشهر';
+$string['xweeks'] = '{$a} أسابيع';
+$string['toomanyrows'] = 'سيقوم التقرير بإرجاع صفوف {$a->total}، الأكبر من الحد الأقصى للمقدار المسموح به من {$a->max}.  في حالة توافر عامل تصفية، يرجى استخدامه في تقليل عدد النتائج.';
+$string['rowsperpage'] = 'صفوف في كل صفحة';
+$string['tablesortedbydesc'] = 'جدول تم فرزه حسب {$a}، تنازليًا.';
+$string['tablesortedbyasc'] = 'جدول تم فرزه حسب {$a}، تصاعديًا.';
+$string['paginatorfirstlabel'] = 'الأول';
+$string['paginatorfirsttitle'] = 'أول صفحة';
+$string['paginatorlastlabel'] = 'الأخير';
+$string['paginatorlasttitle'] = 'آخر صفحة';
+$string['paginatorprevlabel'] = 'السابق';
+$string['paginatorprevtitle'] = 'الصفحة السابقة';
+$string['paginatornextlabel'] = 'التالي';
+$string['paginatornexttitle'] = 'الصفحة التالية';
