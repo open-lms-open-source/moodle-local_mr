16263ef4a136fc532b76d84636dcd486ecadc4fe
David Castro <david.castro@blackboard.com>
2018-04-23 15:30:44 +0000

    INT-13192: Importing ja lang

---
+++ lang/ja/local_mr.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Moodleroomsフレームワーク';
+$string['addedactivity'] = '{$a->modname} は {$a->fullname} により追加されました';
+$string['basichtml'] = '基本HTML';
+$string['deletedactivity'] = '{$a->modname} は {$a->fullname} により削除されました';
+$string['export'] = 'エクスポート : ';
+$string['filter'] = 'フィルタ';
+$string['loadingdotdotdot'] = '読み込み中 ...';
+$string['local_mr_docs'] = 'ドキュメント';
+$string['moodleroomsframework'] = 'Moodleroomsフレームワーク';
+$string['mrframework'] = 'MRフレームワーク';
+$string['nolimit'] = '無制限';
+$string['oneday'] = '1日';
+$string['onemonth'] = '1ヶ月';
+$string['oneweek'] = '1週';
+$string['oneyear'] = '1年';
+$string['plugin-spreadsheet/excel'] = 'XLS';
+$string['plugin-spreadsheet/ods'] = 'ODS';
+$string['plugin-text/csv'] = 'CSV';
+$string['plugin-text/tsv'] = 'TSV';
+$string['popupdocs'] = '新しいウィンドウでドキュメントを開く';
+$string['popuperrors'] = '新しいウィンドウでドキュメントのエラーを開く';
+$string['reportsql'] = 'SQLレポート';
+$string['reset'] = 'リセット';
+$string['standard'] = '標準';
+$string['updatedactivity'] = '{$a->modname} は {$a->fullname} により更新されました';
+$string['xdays'] = '{$a} 日';
+$string['xhours'] = '{$a} 時間前';
+$string['xminutes'] = '{$a} 分';
+$string['xmonths'] = '{$a} ヶ月';
+$string['xweeks'] = '{$a} 週間';
+$string['toomanyrows'] = 'レポートの行数は {$a->total} 行です。これは、最大許容行数の {$a->max} を超えています。フィルタを利用できる場合は、フィルタを使用して、結果の数を減らしてください。';
+$string['rowsperpage'] = '1ページあたりの行数';
+$string['tablesortedbydesc'] = 'テーブルは、{$a} を基準に降順で並べ替えられました。';
+$string['tablesortedbyasc'] = 'テーブルは、{$a} を基準に昇順で並べ替えられました。';
+$string['paginatorfirstlabel'] = '先頭';
+$string['paginatorfirsttitle'] = '先頭のページ';
+$string['paginatorlastlabel'] = '最後';
+$string['paginatorlasttitle'] = '最後のページ';
+$string['paginatorprevlabel'] = '前';
+$string['paginatorprevtitle'] = '前のページ';
+$string['paginatornextlabel'] = '次';
+$string['paginatornexttitle'] = '次のページ';
