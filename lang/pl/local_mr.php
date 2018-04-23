0b7500adba8ddfe48075519ece578c98c59f2f41
David Castro <david.castro@blackboard.com>
2018-04-23 15:41:05 +0000

    INT-13192: Importing pl lang

---
+++ lang/pl/local_mr.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Moodlerooms Framework';
+$string['addedactivity'] = 'Składnik {$a->modname} dodany przez {$a->fullname}';
+$string['basichtml'] = 'Basic HTML';
+$string['deletedactivity'] = 'Moduł {$a->modname} usunięty przez {$a->fullname}';
+$string['export'] = 'Eksportuj: ';
+$string['filter'] = 'Filtr';
+$string['loadingdotdotdot'] = 'Ładowanie...';
+$string['local_mr_docs'] = 'Dokumenty';
+$string['moodleroomsframework'] = 'Moodlerooms Framework';
+$string['mrframework'] = 'MR Framework';
+$string['nolimit'] = 'Bez ograniczeń';
+$string['oneday'] = '1 dzień';
+$string['onemonth'] = '1 miesiąc';
+$string['oneweek'] = '1 tydzień';
+$string['oneyear'] = '1 rok';
+$string['plugin-spreadsheet/excel'] = 'XLS';
+$string['plugin-spreadsheet/ods'] = 'ODS';
+$string['plugin-text/csv'] = 'CSV';
+$string['plugin-text/tsv'] = 'TSV';
+$string['popupdocs'] = 'Otwiera dokumenty w nowym oknie';
+$string['popuperrors'] = 'Otwórz błędy dokumentu w nowym oknie';
+$string['reportsql'] = 'Raport SQL';
+$string['reset'] = 'Reset';
+$string['standard'] = 'Standard';
+$string['updatedactivity'] = 'Składnik {$a->modname} zaktualizowany przez {$a->fullname}';
+$string['xdays'] = '{$a} dni';
+$string['xhours'] = '{$a} godz.';
+$string['xminutes'] = '{$a} min.';
+$string['xmonths'] = '{$a} mies.';
+$string['xweeks'] = '{$a} tyg.';
+$string['toomanyrows'] = 'Raport zwróciłby następującą liczbę wierszy: {$a->total}, co oznacza, że przekroczona została maksymalna dopuszczalna liczba: {$a->max}. Jeśli dostępny jest filtr, użyj go w celu ograniczenia liczby wyników.';
+$string['rowsperpage'] = 'Wierszy na stronę';
+$string['tablesortedbydesc'] = 'Sortowanie tabeli wg {$a}, malejąco.';
+$string['tablesortedbyasc'] = 'Sortowanie tabeli wg {$a}, rosnąco.';
+$string['paginatorfirstlabel'] = 'pierwsza';
+$string['paginatorfirsttitle'] = 'Pierwsza strona';
+$string['paginatorlastlabel'] = 'ostatnia';
+$string['paginatorlasttitle'] = 'Ostatnia strona';
+$string['paginatorprevlabel'] = 'Poprzednia';
+$string['paginatorprevtitle'] = 'Poprzednia strona';
+$string['paginatornextlabel'] = 'Następna';
+$string['paginatornexttitle'] = 'Następna strona';
