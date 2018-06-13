98f36c8b526a7e4a1f6daabc180e34a03a4bba38
David Castro <david.castro@blackboard.com>
2018-06-13 14:04:46 +0000

    INT-13365: Importing tr lang

---
+++ lang/tr/local_mr.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Moodlerooms Çerçevesi';
+$string['addedactivity'] = '{$a->modname}, {$a->fullname} tarafından eklendi';
+$string['basichtml'] = 'Temel HTML';
+$string['deletedactivity'] = '{$a->modname}, {$a->fullname} tarafından silindi';
+$string['export'] = 'Dışa ver: ';
+$string['filter'] = 'Filtre';
+$string['loadingdotdotdot'] = 'Yükleniyor...';
+$string['local_mr_docs'] = 'Belgeler';
+$string['moodleroomsframework'] = 'Moodlerooms Çerçevesi';
+$string['mrframework'] = 'MR Çerçevesi';
+$string['nolimit'] = 'Sınır yok';
+$string['oneday'] = '1 gün';
+$string['onemonth'] = '1 ay';
+$string['oneweek'] = '1 hafta';
+$string['oneyear'] = '1 yıl';
+$string['plugin-spreadsheet/excel'] = 'XLS';
+$string['plugin-spreadsheet/ods'] = 'ODS';
+$string['plugin-text/csv'] = 'CSV';
+$string['plugin-text/tsv'] = 'TSV';
+$string['popupdocs'] = 'Belgeleri yeni pencerede aç';
+$string['popuperrors'] = 'Belge hatalarını yeni pencerede aç';
+$string['reportsql'] = 'SQL’i raporla';
+$string['reset'] = 'Sıfırla';
+$string['standard'] = 'Standart';
+$string['updatedactivity'] = '{$a->modname}, {$a->fullname} tarafından güncelleştirildi';
+$string['xdays'] = '{$a} gün';
+$string['xhours'] = '{$a} saat';
+$string['xminutes'] = '{$a} dakika';
+$string['xmonths'] = '{$a} ay';
+$string['xweeks'] = '{$a} hafta';
+$string['toomanyrows'] = 'Rapor, {$a->total} satır döndürür; bu da izin verilen maksimum {$a->max} tutarından fazla. Bir filtre varsa sonuç sayısını azaltmak için lütfen filtreyi kullanın.';
+$string['rowsperpage'] = 'Sayfa başına satır';
+$string['tablesortedbydesc'] = 'Tablo sıralama ölçütü {$a}, azalan.';
+$string['tablesortedbyasc'] = 'Tablo sıralama ölçütü {$a}, artan.';
+$string['paginatorfirstlabel'] = 'ilk';
+$string['paginatorfirsttitle'] = 'İlk Sayfa';
+$string['paginatorlastlabel'] = 'son';
+$string['paginatorlasttitle'] = 'Son Sayfa';
+$string['paginatorprevlabel'] = 'önceki';
+$string['paginatorprevtitle'] = 'Önceki Sayfa';
+$string['paginatornextlabel'] = 'sonraki';
+$string['paginatornexttitle'] = 'Sonraki Sayfa';
