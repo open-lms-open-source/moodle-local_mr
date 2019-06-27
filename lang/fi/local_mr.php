9e7ed0ff102431a76d3a70073429885c39628b36
David Castro <david.castro@blackboard.com>
2019-06-27 18:16:59 +0000

    INT-14715: Importing fi lang

--- lang/fi/local_mr.php
+++ lang/fi/local_mr.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @copyright  Copyright (c) 2019 Blackboard Inc. (http://www.blackboard.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -63,3 +63,4 @@ $string['paginatorprevlabel'] = 'edellinen';
 $string['paginatorprevtitle'] = 'Edellinen sivu';
 $string['paginatornextlabel'] = 'seuraava';
 $string['paginatornexttitle'] = 'Seuraava sivu';
+$string['privacy:metadata'] = 'Blackboard Open LMS Framework -lisäosa ei tallenna mitään henkilökohtaisia tietoja.';
