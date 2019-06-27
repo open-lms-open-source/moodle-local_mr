8f9c46e18d22824b89da893768d920da5937710f
David Castro <david.castro@blackboard.com>
2019-06-27 18:13:12 +0000

    INT-14715: Importing nl lang

--- lang/nl/local_mr.php
+++ lang/nl/local_mr.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @copyright  Copyright (c) 2019 Blackboard Inc. (http://www.blackboard.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -63,3 +63,4 @@ $string['paginatorprevlabel'] = 'vorige';
 $string['paginatorprevtitle'] = 'Vorige pagina';
 $string['paginatornextlabel'] = 'volgende';
 $string['paginatornexttitle'] = 'Volgende pagina';
+$string['privacy:metadata'] = 'Er worden geen persoonlijke gegevens opgeslagen door de plugin Blackboard Open LMS Framework.';
