45beb6fab208812d75fb006bc5ef3aaf79c37ffb
David Castro <david.castro@blackboard.com>
2019-06-27 18:12:53 +0000

    INT-14715: Importing tr lang

--- lang/tr/local_mr.php
+++ lang/tr/local_mr.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @copyright  Copyright (c) 2019 Blackboard Inc. (http://www.blackboard.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -63,3 +63,4 @@ $string['paginatorprevlabel'] = 'önceki';
 $string['paginatorprevtitle'] = 'Önceki Sayfa';
 $string['paginatornextlabel'] = 'sonraki';
 $string['paginatornexttitle'] = 'Sonraki Sayfa';
+$string['privacy:metadata'] = 'Blackboard Open LMS Çerçevesi eklentisi, kişisel verileri depolamaz.';
