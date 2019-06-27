cde57f652f621a97a89236658f0fabd23ccd2ded
David Castro <david.castro@blackboard.com>
2019-06-27 18:14:03 +0000

    INT-14715: Importing pl lang

--- lang/pl/local_mr.php
+++ lang/pl/local_mr.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @copyright  Copyright (c) 2019 Blackboard Inc. (http://www.blackboard.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -63,3 +63,4 @@ $string['paginatorprevlabel'] = 'Poprzednia';
 $string['paginatorprevtitle'] = 'Poprzednia strona';
 $string['paginatornextlabel'] = 'Następna';
 $string['paginatornexttitle'] = 'Następna strona';
+$string['privacy:metadata'] = 'Wtyczka Blackboard Open LMS Framework nie przechowuje żadnych danych osobowych.';
