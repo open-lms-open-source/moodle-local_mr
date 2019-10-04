abde21a0072d30bdaf528523a86ef499fb6c239d
Sam Chaffee <sam.chaffee@blackboard.com>
2019-10-04 13:05:41 -0600

    INT-15141: Import th lang

---
+++ lang/th/local_mr.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2019 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'ขอบข่ายงาน Blackboard Open LMS';
+$string['addedactivity'] = 'เพิ่ม {$a->modname} โดย {$a->fullname}';
+$string['basichtml'] = 'HTML พื้นฐาน';
+$string['deletedactivity'] = 'ลบ {$a->modname} โดย {$a->fullname}';
+$string['export'] = 'ส่งออก: ';
+$string['filter'] = 'ตัวกรอง';
+$string['loadingdotdotdot'] = 'กำลังโหลด...';
+$string['local_mr_docs'] = 'เอกสาร';
+$string['moodleroomsframework'] = 'ขอบข่ายงาน Blackboard Open LMS';
+$string['mrframework'] = 'ขอบข่ายงาน Bb Open LMS';
+$string['nolimit'] = 'ไม่จำกัด';
+$string['oneday'] = '1 วัน';
+$string['onemonth'] = '1 เดือน';
+$string['oneweek'] = '1 สัปดาห์';
+$string['oneyear'] = '1 ปี';
+$string['plugin-spreadsheet/excel'] = 'XLS';
+$string['plugin-spreadsheet/ods'] = 'ODS';
+$string['plugin-text/csv'] = 'CSV';
+$string['plugin-text/tsv'] = 'TSV';
+$string['popupdocs'] = 'เปิดเอกสารในหน้าต่างใหม่';
+$string['popuperrors'] = 'เกิดข้อผิดพลาดในการเปิดเอกสารในหน้าต่างใหม่';
+$string['reportsql'] = 'รายงาน SQL';
+$string['reset'] = 'รีเซ็ต';
+$string['standard'] = 'มาตรฐาน';
+$string['updatedactivity'] = 'อัปเดต {$a->modname} โดย {$a->fullname}';
+$string['xdays'] = '{$a} วัน';
+$string['xhours'] = '{$a} ชั่วโมง';
+$string['xminutes'] = '{$a} นาที';
+$string['xmonths'] = '{$a} เดือน';
+$string['xweeks'] = '{$a} สัปดาห์';
+$string['toomanyrows'] = 'รายงานจะส่งคืน {$a->total} แถว ซึ่งมากกว่าจำนวนที่อนุญาตสูงสุด {$a->max} หากมีตัวกรอง โปรดใช้ตัวกรองเพื่อลดจำนวนผลลัพธ์';
+$string['rowsperpage'] = 'แถวต่อหน้า';
+$string['tablesortedbydesc'] = 'ตารางเรียงตาม {$a} จากมากไปน้อย';
+$string['tablesortedbyasc'] = 'ตารางเรียงตาม {$a} จากน้อยไปมาก';
+$string['paginatorfirstlabel'] = 'แรก';
+$string['paginatorfirsttitle'] = 'หน้าแรก';
+$string['paginatorlastlabel'] = 'สุดท้าย';
+$string['paginatorlasttitle'] = 'หน้าสุดท้าย';
+$string['paginatorprevlabel'] = 'ก่อนหน้า';
+$string['paginatorprevtitle'] = 'หน้าก่อนหน้า';
+$string['paginatornextlabel'] = 'ถัดไป';
+$string['paginatornexttitle'] = 'หน้าถัดไป';
+$string['privacy:metadata'] = 'ปลั๊กอินขอบข่ายงาน Blackboard Open LMS จะไม่จัดเก็บข้อมูลส่วนบุคคลใดๆ';
