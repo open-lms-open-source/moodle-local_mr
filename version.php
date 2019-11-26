2def01951a109be137c660b9f6dc80e2b78d1b5e
Jonathan Garcia <jonathan.garcia@blackboard.com>
2019-11-26 15:31:23 -0500

    INT-15220: Version and required version bump

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  */
 
 $plugin->version   = 2018080800;
-$plugin->requires  = 2018120300;
+$plugin->requires  = 2019052000;
 $plugin->component = 'local_mr';
-$plugin->release   = '3.6.3';
+$plugin->release   = '3.7.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0;
