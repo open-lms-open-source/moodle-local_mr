4b497a324833543600e7278b75ad9fd2e2e28532
Rafel Becerra <rafael.becerrarodriguez@openlms.net>
2020-10-21 16:39:32 +0000

    INT-16280: Fix some deprecation errors for Open LMS plugins

--- vendor/zend/Zend/Amf/Parse/Resource/MysqlResult.php
+++ vendor/zend/Zend/Amf/Parse/Resource/MysqlResult.php
@@ -50,16 +50,16 @@ class Zend_Amf_Parse_Resource_MysqlResult
      */
     public function parse($resource) {
         $result = array();
-        $fieldcnt = mysql_num_fields($resource);
+        $fieldcnt = mysqli_num_fields($resource);
         $fields_transform = array();
         for($i=0;$i<$fieldcnt;$i++) {
-            $type = mysql_field_type($resource, $i);
+            $type = mysqli_fetch_field_direct($resource, $i);
             if(isset(self::$fieldTypes[$type])) {
-                $fields_transform[mysql_field_name($resource, $i)] = self::$fieldTypes[$type];
+                $fields_transform[mysqli_fetch_field_direct($resource, $i)->name] = self::$fieldTypes[$type];
             }
         }
 
-        while($row = mysql_fetch_object($resource)) {
+        while($row = mysqli_fetch_object($resource)) {
             foreach($fields_transform as $fieldname => $fieldtype) {
                settype($row->$fieldname, $fieldtype);
             }
