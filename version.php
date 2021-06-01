e39bbbffcd712e75cb2bea1b8fd92cfc4993eb75
Fabian Batioja <fabian.batioja@openlms.net>
2021-06-01 22:14:04 +0000

    INT-17116: Bump versions for 3.10

--- version.php
+++ version.php
@@ -21,9 +21,9 @@
  * @author Mark Nielsen
  */
 
-$plugin->version   = 2020061100;
-$plugin->requires  = 2020061500;
+$plugin->version   = 2021060100;
+$plugin->requires  = 2020110900;
 $plugin->component = 'local_mr';
-$plugin->release   = '3.9.3';
+$plugin->release   = '3.10.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0;
