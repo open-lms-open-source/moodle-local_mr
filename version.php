f7542d5def07e883c23b88abb019b0c8b22227d6
Julian Tovar <julian.tovar@openlms.net>
2022-06-06 09:28:47 -0500

    INT-18027: 3.11 MP3 Version bump

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
  * @author Mark Nielsen
  */
 
-$plugin->version   = 2022041300;
+$plugin->version   = 2022060600;
 $plugin->requires  = 2021051700;
 $plugin->component = 'local_mr';
 $plugin->release   = '3.11.3';
