2b79d3840ec9655b7739b1608cc8d8428342d188
Fabian Batioja <fabian.batioja@openlms.net>
2025-03-17 18:35:39 -0500

    INT-20711: Bump version 4.4MP2

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
  * @author Mark Nielsen
  */
 
-$plugin->version   = 2025011000;
+$plugin->version   = 2025031800;
 $plugin->requires  = 2024042200;
 $plugin->component = 'local_mr';
 $plugin->release   = '4.4.4';
