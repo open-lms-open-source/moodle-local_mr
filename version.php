3341bc2a874933c5c536f5cc397c4763ef384356
Rafael Monterroza <Rafael.Monterroza@openlms.net>
2025-07-22 16:27:34 -0500

    INT-21184: Bump versions for 4.5 MP1

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
  * @author Mark Nielsen
  */
 
-$plugin->version   = 2025060500;
+$plugin->version   = 2025072200;
 $plugin->requires  = 2024100700;
 $plugin->component = 'local_mr';
 $plugin->release   = '4.5.4';
